576f6e3d34d53a81ec1e6b2db26f22d67cdefa55
Szymon Szaminko <sz.szaminko@gmail.com>
2022-04-21 14:09:25 +0200

    Organize the code and define firstNumber, operation and secondNumber

--- calculatorConsoleApp/CalculatorConsoleApp/CalculatorConsoleApp/Program.cs
+++ calculatorConsoleApp/CalculatorConsoleApp/CalculatorConsoleApp/Program.cs
@@ -10,6 +10,7 @@ namespace Calculator.ConsoleApp
             string wallFrame = "|                                     |";
             int firstNumber;
             int secondNumber;
+            string operation;
 
             // 1. Show app title
             Console.WriteLine("| - - -  CALCULATOR CONSOLE APP - - - |");
@@ -18,37 +19,41 @@ namespace Calculator.ConsoleApp
             // 2. Ask for first number
             Console.WriteLine("| - - - -  Give first number - - - -  |");
             Console.Write($" First Number:  ");
+
+
+            // 3. Take first number
             firstNumber = Int32.Parse(Console.ReadLine());
             Console.WriteLine();
 
 
-
-            // 3. Take first number
-
-
             // 4. Ask for operation
+            Console.WriteLine("| Chose operation: add|sub|mul|div   |");
+            Console.Write($" Operation:  ");
+
+
+            // 5. Select operation
+            operation = Console.ReadLine();
+            Console.WriteLine();
+
+
+            // 6. Ask for second number
             Console.WriteLine("| - - - -  Give second number - - - - |");
             Console.Write(" Second Number:  ");
             secondNumber = Int32.Parse(Console.ReadLine());
             Console.WriteLine();
 
-
-
-            // 5. Select operation
-
-
-            // 6. Ask for second number
-            ;
-
             // 7. Take second number
+            secondNumber = Int32.Parse(Console.ReadLine());
+            Console.WriteLine();
+
 
 
             // 8. Make calculations
 
             // 9. Show result
-          
 
-           
+
+
             Console.WriteLine("| _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ |");
 
         }
