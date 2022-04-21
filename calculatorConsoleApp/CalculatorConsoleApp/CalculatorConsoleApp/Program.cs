ccf4b0e9fd69b15d0602a7a94a46e3da8fef87d4
Szymon Szaminko <sz.szaminko@gmail.com>
2022-04-21 13:56:39 +0200

    Create a basic structure and take numbers from console

--- calculatorConsoleApp/CalculatorConsoleApp/CalculatorConsoleApp/Program.cs
+++ calculatorConsoleApp/CalculatorConsoleApp/CalculatorConsoleApp/Program.cs
@@ -6,7 +6,51 @@ namespace Calculator.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World, My Console App");
+            //Variables
+            string wallFrame = "|                                     |";
+            int firstNumber;
+            int secondNumber;
+
+            // 1. Show app title
+            Console.WriteLine("| - - -  CALCULATOR CONSOLE APP - - - |");
+            Console.WriteLine();
+
+            // 2. Ask for first number
+            Console.WriteLine("| - - - -  Give first number - - - -  |");
+            Console.Write($" First Number:  ");
+            firstNumber = Int32.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+
+
+            // 3. Take first number
+
+
+            // 4. Ask for operation
+            Console.WriteLine("| - - - -  Give second number - - - - |");
+            Console.Write(" Second Number:  ");
+            secondNumber = Int32.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+
+
+            // 5. Select operation
+
+
+            // 6. Ask for second number
+            ;
+
+            // 7. Take second number
+
+
+            // 8. Make calculations
+
+            // 9. Show result
+          
+
+           
+            Console.WriteLine("| _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ |");
+
         }
     }
 }
