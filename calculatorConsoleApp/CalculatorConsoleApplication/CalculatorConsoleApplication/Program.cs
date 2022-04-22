24138d7aa07f0641f1716df93e23788875584bc2
Szymon Szaminko <sz.szaminko@gmail.com>
2022-04-22 04:31:16 +0200

    Create methods and creat firs functionality

--- calculatorConsoleApp/CalculatorConsoleApplication/CalculatorConsoleApplication/Program.cs
+++ calculatorConsoleApp/CalculatorConsoleApplication/CalculatorConsoleApplication/Program.cs
@@ -8,9 +8,10 @@ namespace Calculator.ConsoleApp
         {
             //Variables
             string wallFrame = "|                                     |";
-            int firstNumber;
+            int firstNumber = 1;
             int secondNumber;
             string operation;
+            int result;
 
             // 1. Show app title
             Console.WriteLine("| - - -  CALCULATOR CONSOLE APP - - - |");
@@ -23,39 +24,98 @@ namespace Calculator.ConsoleApp
 
             // 3. Take first number
             firstNumber = Int32.Parse(Console.ReadLine());
+            checkNumber(firstNumber);
             Console.WriteLine();
 
 
             // 4. Ask for operation
             Console.WriteLine("| Chose operation: add|sub|mul|div   |");
-            Console.Write($" Your Operation:  ");
+            Console.Write($" Operation:  ");
 
 
             // 5. Select operation
             operation = Console.ReadLine();
+            checkOperation(operation);
             Console.WriteLine();
 
 
             // 6. Ask for second number
             Console.WriteLine("| - - - -  Give second number - - - - |");
             Console.Write(" Second Number:  ");
-            secondNumber = Int32.Parse(Console.ReadLine());
-            Console.WriteLine();
+
 
             // 7. Take second number
             secondNumber = Int32.Parse(Console.ReadLine());
+            checkNumber(secondNumber);
             Console.WriteLine();
 
 
-
             // 8. Make calculations
+            result = makeCalculation(firstNumber, operation, secondNumber);
 
             // 9. Show result
+            Console.WriteLine("| - - - - - -  Show result - - - - - -|");
+            Console.WriteLine($" Result: {result}");
+            Console.WriteLine();
+
+            // 10. Try again ?
 
 
 
             Console.WriteLine("| _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ |");
 
+        }
+
+        public static void startCalculator()
+        {
+
+        }
+
+        public static void checkNumber(int number)
+        {
+            if (number.GetType() != typeof(int))
+            {
+                Console.WriteLine("Bad input! You should restart the calculator.");
+                startCalculator();
+            }
+
+           
+        }
+
+        public static void checkOperation(string operation)
+        {
+
+            if (operation != "add" && operation != "sub" && operation != "div" && operation != "mul" )
+            {
+                Console.WriteLine("Bad operation! You should restart the calculator.");
+                startCalculator();
+            } 
+          
+        }
+
+
+        public static int makeCalculation(int firstNumber, string operation, int secondNumber)
+        {
+            int result = 0;
+
+           if(operation == "add")
+            {
+                result = firstNumber + secondNumber;
+            }else if(operation == "sub")
+            {
+                result = firstNumber - secondNumber;
+            }
+            else if(operation == "div")
+            {
+                result = firstNumber / secondNumber;
+            }
+            else if(operation == "mul")
+            {
+                result = firstNumber * secondNumber;
+            }
+
+
+            return result;
         }
     }
 }
