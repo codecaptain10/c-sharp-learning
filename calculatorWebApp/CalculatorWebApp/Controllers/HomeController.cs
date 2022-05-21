cb4d859fa2b2d7781e4805c99b325ccd9b3bb297
Szymon Szaminko <sz.szaminko@gmail.com>
2022-05-21 17:17:15 +0200

    Update file and add gitignore

---
+++ calculatorWebApp/CalculatorWebApp/Controllers/HomeController.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+namespace CalculatorWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private DataTable dataTable = new DataTable();
+        // GET: HomeController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: HomeController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: HomeController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult addNumber()
+        {
+            return addNumber();
+        }
+
+        // POST: HomeController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HomeController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: HomeController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HomeController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: HomeController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+        [HttpPost]
+        public IActionResult Result(string inputVal)
+        {
+            try
+            {
+                var result = Math.Round(Convert.ToDouble(dataTable.Compute(inputVal.Replace(",", "."), "")), 2).ToString();
+
+                return Json(new { success = true, result });
+            }
+            catch (Exception ex)
+            {
+                throw;
+                return Json(new { success = false, message = ex.Message });
+            }
+        }
+
+
+    }
+}
